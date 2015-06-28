f45a1bb2318a3ab048505c5fb9b37adc68134b19
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-06-28 20:48:55 +0900

    Sample追加

---
+++ TokyoGame/Assets/CreateKudamon.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateKudamon : MonoBehaviour {
+
+    private GameObject[] Createnumber;
+    private Vector3[] pos;
+    [SerializeField]
+    GameObject Kudamon;
+    HandController hand_controller_ = null;
+
+    void Awake()
+    {
+        //Kudamon = GameObject.Find("Kudamon1");
+        Createnumber = new GameObject[100];
+        pos = new Vector3[100];
+
+    }
+
+	void Start () 
+    {
+        Create();
+        hand_controller_ = GameObject.Find("HandController").GetComponent<HandController>();
+	}
+	
+	void Update () 
+    {
+        if (hand_controller_.isAttack)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                pos[i].x = Random.Range(-20, 20);
+                pos[i].y = Random.Range(15, 25);
+                pos[i].z = Random.Range(-20, 15);
+                Createnumber[i] = GameObject.Instantiate(Kudamon, pos[i], Quaternion.identity) as GameObject;
+            }
+        }
+	}
+
+    void Create()
+    {
+       for(int i = 0; i < 100; ++i)
+       {
+           pos[i].x = Random.Range(-20, 20);
+           pos[i].y = Random.Range(15, 25);
+           pos[i].z = Random.Range(-20, 15);
+//  Createnumber[i] = GameObject.Instantiate(Kudamon, pos[i], Quaternion.identity)as GameObject;
+           Createnumber[i] = GameObject.Instantiate(Kudamon, pos[i], Quaternion.identity) as GameObject;
+
+
+       }
+    }
+}
---
+++ TokyoGame/Assets/HandControllerConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class HandControllerConfig : MonoBehaviour {
+
+
+    [SerializeField, TooltipAttribute("回した回数")]
+    float turn_num_ = 1.5f;
+
+    [SerializeField, Range(0.0f, 3.14f * 2),TooltipAttribute("認識できる範囲(円弧)")]
+    float min_arc_ = 3.14f * 2;
+
+    [SerializeField, TooltipAttribute("認識できる範囲(半径)")]
+    float min_radius_ = 5.0f;
+
+    public float turnNum { get { return turn_num_; } }
+    public float minArc { get { return min_arc_; } }
+    public float minRadius { get { return min_radius_; } }
+}
